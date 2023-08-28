6ccfd1fc973b98d98091463c98d2fcdb76cbd220
WhiteDream59160 <91155745+WhiteDream59160@users.noreply.github.com>
2023-08-28 23:15:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gunicorn -w 4 "app:app" -b 0.0.0.0:5000 -t 0
-# flask --app app run --debug --host=0.0.0.0 --port=5000 
+# gunicorn -w 4 "app:app" -b 0.0.0.0:5000 -t 0
+flask --app app run --debug --host=0.0.0.0 --port=5000 
